5be0c08063f4b18c197875527bda5fd04d001e64
Que Mnqika <qaqambilemnqika23@gmail.com>
2023-05-16 16:21:38 +0200

    push everything to remote repo

---
+++ script/code.js
@@ -0,0 +1,16 @@
+let firstName = "Que";
+let lastName = "Mnqika";
+let fullName = firstName + lastName;
+
+console.log(`my name is ${fullName}`);
+
+//second question (Q3 & Q4)
+let numberVar = 5;
+console.log(numberVar * 5);
+
+
+//error
+const fname = "Que";
+fname = "Mnqika"
+
+console.log(fname);
